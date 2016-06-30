ce3a33f60855875f5cd82980168d81b30a38c199
William Calderipe <wcalderipe@gmail.com>
2016-06-29 23:04:05 -0300

    Adds queryParser and bodyParser

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ let app = restify.createServer({
   name: 'VamosJuntas'
 });
 
+app.use(restify.queryParser());
+app.use(restify.bodyParser());
+
+
 app.get('/_health', (req, res) => {
   return res.send(200, { online: true });
 });
