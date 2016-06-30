faf6d65d20223bbc77f7c971e2082c7fe0728921
William Calderipe <wcalderipe@gmail.com>
2016-06-30 20:49:01 -0300

    Adds eslint lib and configs

--- src/app.js
+++ src/app.js
@@ -14,7 +14,11 @@ app.get('/_health', (req, res) => {
   return res.send(200, { online: true });
 });
 
-app.post('/reports', requestValidator(reportSchemas.create), reportsController.create);
+app.post(
+  '/reports',
+  requestValidator(reportSchemas.create),
+  reportsController.create
+);
 
 export default app;
 
