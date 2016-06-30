66f0e00aaade44d5832e858e7020170d2effef95
William Calderipe <wcalderipe@gmail.com>
2016-06-30 20:43:38 -0300

    Adds reports controller spec

---
+++ test/unit/controllers/reports_spec.js
@@ -0,0 +1,9 @@
+import sinon from 'sinon';
+import controller from '../../../src/controllers/reports';
+
+describe('controllers:reports', () => {
+  describe('.create', () => {
+    it('should create a new report if validation pass');
+    it('should respond with 201 if a new report was saved with success');
+  });
+});
