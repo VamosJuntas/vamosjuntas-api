2ec15ec2a091b126132cba3192a84e9a758fb43d
William Calderipe <wcalderipe@gmail.com>
2016-07-03 03:52:31 -0300

    Switches between JSON config files to env vars

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,10 @@
-import merge from 'lodash/merge';
+const ENV_VARS = process.env;
+const ENV = ENV_VARS.NODE_ENV || 'development';
 
-const ENV = process.env.NODE_ENV || 'development';
-
-export default merge(require('./default.json'), require(`./${ENV}.json`));
+export default {
+  env: ENV,
+  mongoose: {
+    db: ENV_VARS.VAMOSJUNTAS_DATABASE_URL || 'mongodb://127.0.0.1/vamosjuntas'
+  }
+}
 
