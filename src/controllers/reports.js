395841e52214bf5f94126e676abf54ccf5475478
William Calderipe <wcalderipe@gmail.com>
2016-07-08 22:12:26 -0300

    Adds create report route

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -1,5 +1,16 @@
-function create(req, res) {
-  return res.send(201);
+import HttpStatus from 'http-status-codes';
+import Report from '../models/report';
+
+function create(req, res, next) {
+  let report = new Report(req.body);
+  report.saveAsync()
+    .then((document) => {
+      return res.send(HttpStatus.CREATED, document);
+    })
+    .catch((err) => {
+      return next(err);
+    });
 }
 
 export default { create };
+
--- test/unit/controllers/reports_spec.js
+++ test/unit/controllers/reports_spec.js
@@ -1,9 +1,87 @@
+import HttpStatus from 'http-status-codes';
 import sinon from 'sinon';
+import Report from '../../../src/models/report';
 import controller from '../../../src/controllers/reports';
 
 describe('controllers:reports', () => {
+  function createReqParams() {
+    return {
+      geolocation: [-30.057389, -51.174544],
+      category: 'lorem',
+      date: '07/02/2016'
+    };
+  }
+
+  function createReportDocument() {
+    return {
+      _id: '123lorem',
+      __v: 0,
+      geolocation: [-30.057389, -51.174544],
+      category: 'lorem',
+      date: '07/02/2016',
+      createdAt: '2016-07-09T00:28:43.306Z',
+      updatedAt: '2016-07-09T00:28:43.306Z'
+    };
+  }
+
   describe('.create', () => {
-    it('should create a new report if validation pass');
-    it('should respond with 201 if a new report was saved with success');
+    context('valid', () => {
+      let req, res, report, saveAsyncPromise;
+
+      beforeEach(() => {
+        report = createReportDocument();
+
+        saveAsyncPromise = Promise.resolve(report);
+        sinon.stub(Report.prototype, 'saveAsync').returns(saveAsyncPromise);
+
+        req = {
+          params: createReqParams()
+        }
+
+        res = {
+          send: sinon.spy()
+        }
+      });
+
+      afterEach(() => {
+        Report.prototype.saveAsync.restore();
+      });
+
+      it('should call res.send with 201 and report args', () => {
+        controller.create(req, res);
+        return expect(saveAsyncPromise).to.be.fulfilled.then(() => {
+          expect(res.send).to.be.calledWith(HttpStatus.CREATED, report);
+        });
+      });
+    });
+
+    context('invalid', () => {
+      let req, next, mongooseError, saveAsyncPromise;
+
+      beforeEach(() => {
+        mongooseError = new Error('validation error');
+        saveAsyncPromise = Promise.reject(mongooseError);
+        sinon.stub(Report.prototype, 'saveAsync').returns(saveAsyncPromise);
+
+        let params = createReqParams();
+        delete params.geolocation;
+        req = {
+          params: params
+        };
+
+        next = sinon.spy();
+      });
+
+      afterEach(() => {
+        Report.prototype.saveAsync.restore();
+      });
+
+      it('should call next with error data if mongoose validation fails', () => {
+        controller.create(req, {}, next);
+        return expect(saveAsyncPromise).to.be.rejected.then(() => {
+          expect(next).to.be.calledWith(mongooseError);
+        });
+      });
+    });
   });
 });
