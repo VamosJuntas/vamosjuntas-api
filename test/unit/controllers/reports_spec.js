2ca64bd3ae84f79006be8ba0c57498c0abe4c8f1
Josi Gama <josyvisa@gmail.com>
2016-08-04 22:51:58 -0300

    @josigama @viniciusgama #3 - change the contract schema for the geolocation to be a object

--- test/unit/controllers/reports_spec.js
+++ test/unit/controllers/reports_spec.js
@@ -1,10 +1,2 @@
 import sinon from 'sinon';
 import controller from '../../../src/controllers/reports';
-
-describe('controllers:reports', () => {
-  describe('.create', () => {
-    it('should create a new report if validation pass');
-    it('should respond with 201 if a new report was saved with success');
-    it('should');
-  });
-});
