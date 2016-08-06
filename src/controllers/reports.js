7929889fa46036de1fdd952472cc930f62e14f50
Josi Gama <josyvisa@gmail.com>
2016-08-06 16:58:24 -0300

    @josigama @vinigama #2-save report risk in DB

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -1,5 +1,17 @@
-function create(req, res) {
-  return res.send(201);
+import Report from './../models/report';
+
+function create(request, response, next) {
+  var params = {
+    geolocation: [request.params.geolocation.latitude, request.params.geolocation.longitude],
+    category: request.params.category,
+    date: request.params.date
+  }
+  var report = Report.create(params).then(function() {
+    response.send(201)
+  }).catch(function() {
+    console.log("nao criou")
+  });
+  return next()
 }
 
 export default { create };
--- test/unit/controllers/reports_spec.js
+++ test/unit/controllers/reports_spec.js
@@ -1,2 +1,36 @@
-import sinon from 'sinon';
-import controller from '../../../src/controllers/reports';
+import sinon from 'sinon'
+import chai, { assert } from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+
+import controller from '../../../src/controllers/reports'
+import Report from '../../../src/models/report'
+
+chai.use(chaiAsPromised)
+chai.should()
+
+describe('Reports Controller', function() {
+  it('should create a new Report and return success', function(done) {
+    var mock = {
+      next: sinon.spy(),
+      response: { send: sinon.spy() },
+      request: {
+        params: {
+          geolocation: {
+            latitude: 10,
+            longitude: 20
+          },
+          category: "Roubo",
+          date: "10/10/2016"
+        }
+      }
+    };
+    var success = Promise.resolve();
+    sinon.stub(Report, 'create').returns(success)
+    controller.create(mock.request, mock.response, mock.next);
+
+    success.should.be.fulfilled.then(function () {
+        assert(mock.response.send.calledWith(201), 'response.send was never called')
+        assert(mock.next.calledOnce, 'next was never called')
+    }).should.notify(done)
+  })
+})
