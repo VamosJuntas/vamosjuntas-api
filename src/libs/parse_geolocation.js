ed42d6dc3d4a4e0fab93013c3115cf82d3c07b90
othman853 <anuar.yasser@gmail.com>
2016-10-19 19:12:40 -0200

    @othman853 Simplifying parse_geolocation

--- src/libs/parse_geolocation.js
+++ src/libs/parse_geolocation.js
@@ -1,19 +1,12 @@
 export default function parseGeolocation(value) {
 
-  if (!value) {
-    return undefined;
-  }
-
-  if (typeof value !== 'string') {
-    return undefined;
-  }
-
-  const points = value
-                      .split(',')
-                      .filter(p => p !== '')
-                      .map(p => p.trim())
-                      .map(Number)
-                      .filter(p => !isNaN(p));
+  const points = (typeof value !== 'string')
+  ? []
+  : value
+          .split(',')
+          .filter(p => p !== '')
+          .map(p => Number(p.trim()))
+          .filter(p => !isNaN(p));
 
   return points.length !== 2 ? undefined : points;
 
